f061992847aa72032e162f25c9bd19753265678a
Serg1K <amsher2007@mail.ru>
2024-04-13 11:53:40 +1000

    brt service

---
+++ brt-service/src/main/java/com/nexign/babybilling/brtservice/BrtServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nexign.babybilling.brtservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrtServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrtServiceApplication.class, args);
+    }
+
+}
