002e9b068fea37083313c98410a51850d767308b
Serg1K <amsher2007@mail.ru>
2024-05-03 16:11:43 +1000

    [brt] auto pay

---
+++ brt-service/src/main/java/com/nexign/babybilling/brtservice/service/CronService.java
@@ -0,0 +1,34 @@
+package com.nexign.babybilling.brtservice.service;
+
+import com.nexign.babybilling.brtservice.entity.Customer;
+import db.migration.V2__InitData;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CronService {
+
+    private final CustomerService customerService;
+
+    @Scheduled(fixedDelay = 5000L)
+    @Transactional
+    public void payment() {
+        //пополним баланс раномным абонентам
+        for(int i =0; i < 10; i++) {
+            String phone = V2__InitData.phones.get((int) (Math.random() * V2__InitData.phones.size()));
+            Customer byMsisnd = customerService.findByMsisnd(phone);
+            int amount = (int) (Math.random() * 1000 + 100);
+            byMsisnd.setBalance(BigDecimal.valueOf(amount));
+
+            customerService.save(byMsisnd);
+            log.info("Пополнен баланс для {} на сумму {}", byMsisnd.getMsisnd(), amount);
+        }
+    }
+}
